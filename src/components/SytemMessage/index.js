f181ada32058164b581d4f2e75f3678c937aa282
Mubarak Show <mubarak.show@gmail.com>
2020-09-25 06:42:15 +0100

    minor fixes

--- src/components/SytemMessage/index.js
+++ src/components/SytemMessage/index.js
@@ -1,18 +1,22 @@
 import React from "react";
-import Button from '../Button'
-import './style.scss'
+import Button from "../Button";
+import "./style.scss";
 
 const SystemMessage = (props) => {
   return (
     <div className="sysmsg">
-      <div className="sysmsg__container" style={{background: `linear-gradient(49deg, ${props.theme})`}}>
-        <p className="sysmsg__title">
-          {props.title} <span>{props.emoji}</span>
-        </p>
+      <div
+        className="sysmsg__container"
+        style={{ background: `linear-gradient(49deg, ${props.theme})` }}
+      >
+        <div className="sysmsg__header">
+          <p className="sysmsg__header__title">{props.title}</p>
+          <span className="sysmsg__header__emoji">{props.emoji}</span>
+        </div>
         <p className="sysmsg__description">{props.description}</p>
         <Button
           cta={props.cta}
-          theme={props.theme.split(', ')[1].split(' ')[0]}
+          theme={props.theme.split(", ")[1].split(" ")[0]}
         />
       </div>
     </div>
